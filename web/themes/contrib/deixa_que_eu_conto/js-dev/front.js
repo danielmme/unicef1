261ab9686d5c7de04cfd4c431fb7e7fe975e9240
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-06-05 18:00:09 -0300

    home sections

--- web/themes/contrib/deixa_que_eu_conto/js-dev/front.js
+++ web/themes/contrib/deixa_que_eu_conto/js-dev/front.js
@@ -42,6 +42,77 @@
       });
 
     }
+    // home amazonia
+    if($('.home-amazonia').length){
+      console.log('has home amazonia');
+      $('.home-amazonia .slider-item-inner').each(function(){
+        var title = $(this).attr('data-title');
+
+        var number = title.match(/\d/gm).join().replace(',','')
+        $(this).children('a').children('.programa').html(number);
+        $(this).children('a').children('.title').html(title.replace('Programa ','').replace(number,'').replace(' – ',''));
+      })
+
+      $('.home-gerais .programas_amazonia .view-content').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+            }
+          }
+        ]
+      });
+
+    }
+
+    // home amazonia
+    if($('.home-afro').length){
+      console.log('has home afro');
+      $('.home-afro .slider-item-inner').each(function(){
+        var title = $(this).attr('data-title');
+
+        var number = title.match(/\d/gm).join().replace(',','')
+        $(this).children('a').children('.programa').html(number);
+        $(this).children('a').children('.title').html(title.replace('Programa ','').replace(number,'').replace(' – ',''));
+      })
+
+      $('.home-afro .programas-afro .view-content').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+            }
+          }
+        ]
+      });
+
+    }
 
     // scroll animations
     AOS.init();
