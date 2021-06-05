fc405786f1d9e889f7aedfadb3696c90cca21e85
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-06-05 17:01:54 -0300

    home gerais

--- web/themes/contrib/deixa_que_eu_conto/js-dev/front.js
+++ web/themes/contrib/deixa_que_eu_conto/js-dev/front.js
@@ -7,5 +7,42 @@
     $($inputBtn).addClass('show');
     $('.form-type-textfield').append($inputBtn);
 
+    // home gerais
+    if($('.home-gerais').length){
+      console.log('has home gerais');
+      $('.home-gerais .slider-item-inner').each(function(){
+        var title = $(this).attr('data-title');
+
+        var number = title.match(/\d/gm).join().replace(',','')
+        $(this).children('a').children('.programa').html(number);
+        $(this).children('a').children('.title').html(title.replace('Programa ','').replace(number,'').replace(' – ',''));
+      })
+
+      $('.home-gerais .programas-gerais .view-content').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+            }
+          }
+        ]
+      });
+
+    }
+
+    // scroll animations
     AOS.init();
 })();
