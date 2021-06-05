0bf7e9d5c3d3c8636b0a0dd1a18776aa62a92e1e
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-06-05 04:18:29 -0300

    js files

--- web/themes/contrib/deixa_que_eu_conto/js-dev/main.js
+++ web/themes/contrib/deixa_que_eu_conto/js-dev/main.js
@@ -1,26 +1,3 @@
 (function(){
-  console.log('frn');
-  document.querySelector('.carousel-inner > div:first-child').classList.add('active');
-  const carouselInner = document.querySelector('.carousel-inner').cloneNode(true);
-
-  document.getElementById('carouselExampleControls').insertBefore(carouselInner, document.getElementById('carouselExampleControls').firstChild);
-
-  const calendarElement = document.getElementById('carouselExampleControls').cloneNode(true);
-  const frontSupList = document.querySelector('.front-sup-list');
-
-  const frontCalendarContainer = document.createElement('div');
-
-  frontCalendarContainer.classList.add('col-lg-4');
-  frontCalendarContainer.classList.add('col-sm-6');
-  frontCalendarContainer.classList.add('mb-4');
-  frontCalendarContainer.classList.add('d-flex');
-  frontCalendarContainer.classList.add('calendar-card');
-  frontCalendarContainer.classList.add('cloned');
-
-
-  frontCalendarContainer.appendChild(calendarElement);
-  frontSupList.appendChild(frontCalendarContainer);
-
-  // const calendarToRemove = document.querySelector('.front-cards-calendar .calendar-card') ;
-  // calendarToRemove.parentNode.removeChild(calendarToRemove);
+  console.log('main');
 })();
