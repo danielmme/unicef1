fe05a07080ab0da4bb8829aed1632e1f929741db
Daniel MMe <medeiros.danielm@gmail.com>
2021-06-18 15:18:19 -0400

    small layout fixes

--- web/themes/contrib/deixa_que_eu_conto/js-dev/main.js
+++ web/themes/contrib/deixa_que_eu_conto/js-dev/main.js
@@ -1,24 +1,2 @@
 (function(window, document, $){
-  // console.log('main');
-  // // select
-  // var $selectFakeContainer = document.createElement('div');
-  // $($selectFakeContainer).addClass('select-fake-container');
-  // $('.form-item-tipo-de-atividades').append($selectFakeContainer);
-  // $('.form-item-tipo-de-atividades select option').each(function(){
-  //   var $optionFake = document.createElement('div');
-  //   var value = $(this).attr('value');
-  //   var text = $(this).text();
-  //   console.log(value);
-  //   console.log(text);
-  //   $($optionFake).addClass('option-fake')
-  //   $($optionFake).attr('value',value);
-  //   $($optionFake).text(text);
-  //   $($selectFakeContainer).append($optionFake);
-  // })
-  // $('.option-fake').on('click',function(){
-  //   var thisValue = $(this).attr('value')
-  //   console.log(thisValue);
-  //   $(this).addClass('checked')
-  //   $('.form-item-tipo-de-atividades select option[value="'+thisValue+'"]').attr('selected',true)
-  // })
 })(window, document, jQuery);
